4d5189477ad0f2045787f85e6a895b74b536c3de
Franceq34 <quentin.france@free.fr>
2018-01-04 12:22:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
     session_start();
     $d_s = DIRECTORY_SEPARATOR;
     require_once(__DIR__ . $d_s . "lib" . $d_s . "file.php");
-    require_once(File::build_path(array("controller", "routeur.php")))
+    require_once(File::build_path(array("controller", "routeur.php")));
